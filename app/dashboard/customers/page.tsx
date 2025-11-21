5f80f129eebf8b617f92b52f187e5b0770f66096
Talitha <lalacandycanes@gmail.com>
2025-11-21 14:47:24 +1300

    chapter 14 flop with types of react/-dom for use action/form state - customer search not work but page good

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,18 @@
 import { fell } from '@/app/ui/fonts';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
-export default function Page() {
+export default async function Page() {
+const query = '';
+  const [customers] = await Promise.all([
+          fetchFilteredCustomers(query),
+        ]);
     return (
     <main>
       <strong><h1 className={`${fell.className} text-cyan-500 mb-4 text-xl md:text-2xl`}> Customers Page </h1></strong>
-      
+
+      <CustomersTable customers={customers}/>
     </main>
     )
   }
+  //sorta working cause im not going to finish this course with the use action state issues
